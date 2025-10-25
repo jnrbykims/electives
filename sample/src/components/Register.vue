96fa60f27887a3dd3bd39f3506637ee771ac8c11
jam <jalyxasjam@gmail.com>
2025-10-25 14:16:31 +0800

    init

---
+++ sample/src/components/Register.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>Register Component</div>
+
+    <div>
+      <b>First Name:</b>
+      <input type="text" v-model="firstName" />
+    </div>
+
+    <div>
+      <b>Last Name:</b>
+      <input type="text" v-model="lastName" />
+    </div>
+
+    <button @click="register">Register</button>
+
+    <div>
+      {{ displayName }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      firstName: "jam",
+      lastName: "Estelloso"
+    };
+  },
+  computed: {
+    displayName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  },
+  methods: {
+    register() {
+      alert(this.displayName);
+    },
+    goToLogin() {
+      alert("go to login page");
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  margin: 8px 0;
+}
+button {
+  margin-top: 10px;
+}
+</style>
